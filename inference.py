9dfc7ca001dc20b1fd241b9107ca2562ec19765a
tirsaiwalamubin <66933086+tirsaiwalamubin@users.noreply.github.com>
2021-02-12 13:23:41 +0530

    update inference.py

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from PIL import Image
 
 model = tf.keras.models.load_model('./baseline_model.h5')
 
@@ -12,14 +13,16 @@ classes = 	{0 : 'Bacterial Spot',
 			6 : 'Spider Mite Damage',
 			7 : 'Tomato Yellow Leaf Curl'}
 
-def predict(image):
-	"""
-	Just Give an RGB image
-	"""
-	image = tf.image.resize(image, [320,320])
-	image = image.numpy()[:,:,3]
-	image = np.expand_dims(image, axis = 0)
-	prediction = model.predict(image)
-	prediction = np.argmax(prediction)
-	prediction = classes[prediction]
-	return prediction
+def predict(image_path):
+    """
+    Just Give an image path
+    """
+    image = Image.open(image_path)
+    image = np.array(image)
+    image = tf.image.resize(image, [320,320])
+    image = image.numpy()[:,:,:3]
+    image = np.expand_dims(image, axis = 0)
+    prediction = model.predict(image)
+    prediction = np.argmax(prediction)
+    prediction = classes[prediction]
+    return prediction
